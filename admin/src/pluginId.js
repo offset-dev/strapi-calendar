25212a87f6de99ebe7b8726f14920049ff2a6800
Luis Rodriguez <luisrodriguez.7@icloud.com>
2022-05-30 12:29:14 -0500

    update name

--- admin/src/pluginId.js
+++ admin/src/pluginId.js
@@ -1,5 +1,5 @@
 const pluginPkg = require('../../package.json');
 
-const pluginId = pluginPkg.strapi.displayName.replace(/^(@[^-,.][\w,-]+\/|strapi-)plugin-/i, '');
+const pluginId = pluginPkg.strapi.name.replace(/^(@[^-,.][\w,-]+\/|strapi-)plugin-/i, '');
 
 module.exports = pluginId;
